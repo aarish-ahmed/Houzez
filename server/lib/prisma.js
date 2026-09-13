aab236d4a2a6da803ea11e72905683cb41fe2fe0
aarish <aarishahmed96@gmail.com>
2026-09-14 00:25:08 +0600

    resolve Render deployment crash by using standard @prisma/client

--- server/lib/prisma.js
+++ server/lib/prisma.js
@@ -1,10 +1,7 @@
-import { PrismaClient } from "../lib/generated/prisma/client.ts";
+import { PrismaClient } from "./generated/prisma/client.js";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-
-const adapter=new PrismaPg({connectionString:process.env.DATABASE_URL})
-const prisma= new PrismaClient({adapter})
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
-
-
